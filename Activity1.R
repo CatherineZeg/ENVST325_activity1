a23b407b70c85fe8b388e87c9620ab559b592019
CatherineZeg <czeglars@hamilton.edu>
2026-01-29 16:44:40 +0000

    finished in class prompts

--- Activity1.R
+++ Activity1.R
@@ -32,6 +32,19 @@ highPeaks$elev
 
 highPeaks[,1]
 
+#snowfall conversion - prompt 1
+snowfall_inch <- c(2.5, 3, 5, 4.5)
+snowfall_cm <- snowfall_inch * 2.54
+
+#highpeaks df - prompt 2
+highPeaks_df <- data.frame(Name = c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak"),
+                           Elevation_ft = c(4960, 4926, 4857, 4840),
+                           Climb_Ascent_ft = c(3570, 4265, 2800, 4178),
+                           Round_trip_length_mi = c(17.8, 17.9, 13.2, 16))
+
+#math - prompt 3
+celsius <- c(-44, 0, 20, 35) 
+fahrenheit <- celsius * 9/5 + 32
 
 #homework ----
 
